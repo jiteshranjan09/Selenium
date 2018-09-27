d54e2a385bcb73399b57bf70d6cd5019650a9f92
Jitesh Ranjan <Jitesh Ranjan@Excelance-OHD>
2018-09-28 01:09:36 +0530

    commit

--- src/main/java/com/smart/qa/page/BlankInvoicePage.java
+++ src/main/java/com/smart/qa/page/BlankInvoicePage.java
@@ -4,10 +4,13 @@ public class BlankInvoicePage {
 
 	public void Test1(){
 		System.out.println("DELL");
+		System.out.println("Iball");
+		System.out.println("Iball");
 	}
 	public void Test() {
 		System.out.println("Iball");
 		System.out.println("Iball");
+		System.out.println("DELL");
 	}
 	
 }
